15be2af69d5af44a3128a63630f6902a43840d6b
AbdulHafez <abodeku508@gmail.com>
2024-02-25 17:40:51 +0300

    Uploading the project

---
+++ database/migrations/2024_02_25_135625_create_properties_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('category_id')->constrained();
+            $table->foreignId('street_id')->constrained();
+            $table->float('price');
+            $table->integer('floor');
+            $table->smallInteger('number_of_rooms');
+            $table->smallInteger('number_of_kitchens');
+            $table->smallInteger('number_of_bathrooms');
+            $table->float('space');
+            $table->boolean('rent');
+            $table->timestamps();
+
+        });
+        Schema::enableForeignKeyConstraints();
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('properties');
+    }
+};
---
+++ database/migrations/2024_02_25_135821_create_image__properties_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::create('image__properties', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('property_id')->constrained();
+            $table->string('image');
+            $table->timestamps();
+        });
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('image__properties');
+    }
+};
