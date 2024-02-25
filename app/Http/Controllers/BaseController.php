a44583f01ac3cc202b1c05685d13c78bd8fd992a
AbdulHafez <abodeku508@gmail.com>
2024-02-25 18:16:52 +0300

    Adding BaseController

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class BaseController extends Controller
+{
+    public function sendResponse($result , $date_name = "data"): JsonResponse
+    {
+            $response = [
+                'status' => 'success' ,
+                $date_name => $result
+            ];
+
+            return response()->json($response , '200');
+    }
+
+    public function sendError($errorMessage = [] , $code = 200): JsonResponse
+    {
+            $response = [
+                'status' => 'failure'
+            ];
+
+            if(!empty($errorMessage))
+            {
+                $response['error_message'] = $errorMessage ;
+            }
+            return response()->json($response , $code);
+    }
+
+}
