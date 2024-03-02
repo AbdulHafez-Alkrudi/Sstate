de2269e58a75c484a97759961808d75bc8c3d956
AbdulHafez <abodeku508@gmail.com>
2024-03-02 15:35:34 +0300

    declaring guarded array in Image_property model

--- app/Models/Image_Property.php
+++ app/Models/Image_Property.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Image_Property extends Model
 {
     use HasFactory;
+    protected $guarded = [] ;
 
     public function property(): BelongsTo
     {
