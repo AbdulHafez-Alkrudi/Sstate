82055f242f083f69668b5df04581369a54a5023b
AbdulHafez <abodeku508@gmail.com>
2024-03-17 16:26:29 +0300

    Adding show function in property controller

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Image_Property;
 use App\Models\Property;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use phpseclib3\Math\PrimeField\Integer;
 
@@ -12,7 +17,7 @@ class PropertyController extends BaseController
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): JsonResponse
     {
         $properties = Property::query()->filter(request(['street','category' ,'for_rent' , 'class' , 'space']))->get();
         return $this->sendResponse($properties);
@@ -23,6 +28,7 @@ class PropertyController extends BaseController
      */
     public function store(Request $request)
     {
+        DB::beginTransaction();
         $input = $request->all();
 
         $validator = Validator::make($input , [
@@ -34,17 +40,31 @@ class PropertyController extends BaseController
             'number_of_kitchens'  => 'required',
             'number_of_bathrooms' => 'required',
             'space'               => 'required',
-            'rent'                => 'required',
-            'images'              => 'required|'
+            'for_rent'            => 'required',
+            'images'              => 'array|present',
         ]);
 
-
-
         if($validator->fails()){
+            DB::rollBack();
             return $this->sendError($validator->errors());
         }
 
-        Property::create($input);
+        $input['user_id'] = Auth::id();
+        $property = Property::create(Arr::except($input , ['images']));
+
+        foreach($request['images'] as $image){
+            $image_name = time().'.'.$image->getClientOriginalExtension();
+            $image->move(public_path('images/properties_images') , $image_name);
+            $image_name = 'images/properties_images/'.$image_name;
+
+            Image_Property::create([
+               'property_id' => $property['id'],
+               'image' => $image_name
+            ]);
+
+        }
+        DB::commit();
+        return $this->sendResponse([]);
     }
 
     /**
@@ -52,26 +72,23 @@ class PropertyController extends BaseController
      */
     public function show($id)
     {
-        $property = Property::query()->where('id' , $id)->get();
-        $property->update('popularity' , $property['popularity'] + 1) ;
+        $property = Property::find($id);
+        $property->update( ['popularity' => $property['popularity'] + 1] ) ;
+        $images_paths = Image_Property::query()->where('property_id' , $id)->get();
 
+        $images = array();
+        foreach($images_paths as $path){
+            $images[]= base64_encode(file_get_contents(public_path($path['image'])));
+        }
+        $property['images'] = $images ;
         return $this->sendResponse($property);
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, string $id)
     {
-        //
+
     }
 
     /**
